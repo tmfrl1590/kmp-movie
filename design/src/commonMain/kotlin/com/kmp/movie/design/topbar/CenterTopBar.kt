f75158b0a7dea23100b64e70a7e7774ae80f4ddc
seulgi <tmfrl1590@gmail.com>
2025-06-03 23:01:02 +0900

    Feat: 영화 상세 화면에 TopBar 및 뒤로가기 기능 추가

---
+++ design/src/commonMain/kotlin/com/kmp/movie/design/topbar/CenterTopBar.kt
@@ -0,0 +1,22 @@
+package com.kmp.movie.design.topbar
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CenterTopBar(
+    title: @Composable (() -> Unit),
+    navigationIcon: @Composable (() -> Unit),
+){
+    CenterAlignedTopAppBar(
+        title = title,
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.Black
+        ),
+        navigationIcon = navigationIcon,
+    )
+}
--- presentation/src/commonMain/kotlin/com/kmp/movie/presentation/ui/detail/MovieDetailScreenRoute.kt
+++ presentation/src/commonMain/kotlin/com/kmp/movie/presentation/ui/detail/MovieDetailScreenRoute.kt
@@ -5,8 +5,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,6 +22,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import com.kmp.movie.design.topbar.CenterTopBar
 import com.kmp.movie.presentation.ui.detail.component.MovieCreditArea
 import com.kmp.movie.presentation.ui.detail.component.MovieImage
 import com.kmp.movie.presentation.ui.detail.component.MovieInfoArea
@@ -38,18 +45,39 @@ fun MovieDetailScreenRoute(
 
     MovieDetailScreen(
         state = state,
+        onGotoNavigateBack = { navController.popBackStack() }
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun MovieDetailScreen(
     state: MovieDetailState,
+    onGotoNavigateBack: () -> Unit,
 ){
     val scrollState = rememberScrollState()
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
         ,
+        topBar = {
+            CenterTopBar(
+                title = {},
+                navigationIcon = {
+                    IconButton(
+                        onClick = onGotoNavigateBack,
+                    ){
+                        Icon(
+                            imageVector = Icons.Default.KeyboardArrowLeft,
+                            contentDescription = "back",
+                            modifier = Modifier
+                                .size(32.dp),
+                            tint = Color.White
+                        )
+                    }
+                }
+            )
+        },
         containerColor = Color.Black,
     ) {
         Column(
