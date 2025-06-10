0226a798556870f11af941b02433861e779116cb
seulgi <tmfrl1590@gmail.com>
2025-06-10 22:35:27 +0900

    Feat: 이미지 로딩 실패했을 때 디폴트 텍스트 나오도록 추가

--- design/src/commonMain/kotlin/com/kmp/movie/design/AsyncImage.kt
+++ design/src/commonMain/kotlin/com/kmp/movie/design/AsyncImage.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -34,5 +36,17 @@ fun AsyncImage(
                 CircularProgressIndicator()
             }
         },
+        error = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "No Image",
+                    color = Color.White
+                )
+            }
+        }
     )
 }
