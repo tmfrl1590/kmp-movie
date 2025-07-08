65e6a25cc703f71b6e9637ecccd87ae733c63d62
seulgi <tmfrl1590@gmail.com>
2025-07-08 21:07:48 +0900

    Release: verson 1.0.1

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -81,8 +81,8 @@ android {
         applicationId = "com.kmp.movie"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
     }
     packaging {
         resources {
