fd1fbcd471cbc9dd52114a9cae0fcd86752f2218
seulgi <tmfrl1590@gmail.com>
2025-09-28 23:01:39 +0900

    Release: version 1.1.0

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -84,8 +84,8 @@ android {
         applicationId = "com.kmp.movie"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 11
-        versionName = "1.0.9"
+        versionCode = 12
+        versionName = "1.1.0"
     }
     packaging {
         resources {
